828e3593785fdb3da94eb786bf52754ed0955730
rafaelfrz <rafaelferrazswd2@protonmail.com>
2023-06-11 20:06:53 -0300

    menu inicial

--- menu.gd
+++ menu.gd
@@ -8,8 +8,7 @@ func _ready():
 	$VBoxContainer/HBoxContainer/VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file
-	("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_button_pressed():
 	pass
